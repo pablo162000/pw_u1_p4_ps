96e981d9551d2cc8b82cba85596e7fa71ced46a2
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-24 19:44:19 -0500

    taller9

---
+++ js/calculadora.js
@@ -0,0 +1,55 @@
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+
+function restar(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function dividir(num1, num2) {
+    return num1 / num2;
+}
+
+
+
+
+function sumarNumeros() {
+    var numero1 = convertir('idNum1');
+    var numero2 = convertir('idNum2');
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + sumar(numero1, numero2);
+}
+
+function restarNumeros() {
+    var numero1 = convertir('idNum1');
+    var numero2 = convertir('idNum2');
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + restar(numero1, numero2);
+}
+function multiplicarNumeros() {
+    var numero1 = convertir('idNum1');
+    var numero2 = convertir('idNum2');
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + multiplicar(numero1, numero2);
+}
+function dividirNumeros() {
+    var numero1 = convertir('idNum1');
+    var numero2 = convertir('idNum2');
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + dividir(numero1, numero2);
+}
+
+function convertir(idCampo) {
+    return parseInt(document.getElementById(idCampo).value);
+
+}
+
+function eliminarElemento() {
+    document.getElementById('idEliminar').remove;
+
+}
+
+function insertarElemento() {
+    document.getElementById('idElemento').innerHTML = '<strong>Impotante</strong>';
+
+
+}
